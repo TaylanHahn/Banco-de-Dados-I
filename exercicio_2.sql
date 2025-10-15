78e780dfc92bffb34a6b4978f06f61c4471ea1d1
Taylan <146947181+TaylanHahn@users.noreply.github.com>
2025-10-15 19:52:11 -0300

    Update exercicio_2.sql

--- exercicio_2.sql
+++ exercicio_2.sql
@@ -1,6 +1,6 @@
 Criação dos INSERTS do exercicio 01
 -----------------------------------------------------------------------------------------
-# insert CLiente
+# CLiente
 
 insert into cliente(id_cliente, nome, endereco, telefone)
   values(s_cliente.nextval, 'Taylan', 'Rua Blabla', '51888877777');
@@ -11,7 +11,7 @@ insert into cliente(id_cliente, nome, endereco, telefone)
 insert into cliente(id_cliente, nome, endereco, telefone)
   values(s_cliente.nextval, 'Kannemann', 'Rua Gremio', '5100001111');
 
-# insert Produto
+# Produto
 
 insert into produto(id_produto, nome, preco, quantidade_estoque)
   values(s_produto.nextval, 'PC Gamer', 15000, 25);
@@ -23,7 +23,7 @@ insert into produto(id_produto, nome, preco, quantidade_estoque)
   values(s_produto.nextval, 'MacBook', 10000, 13);
 
 
-# insert Pedido
+# Pedido
 insert into pedido(id_pedido, id_cliente, data_pedido, data_entrega)
   values();
 
@@ -35,7 +35,7 @@ insert into pedido(id_pedido, id_cliente, data_pedido, data_entrega)
 
 
 
-# insert Pedido_produto
+# Pedido_produto
 
 insert into pedido_produto(id_pedido, id_produto, quantidade, valor_venda)
   values(s_pedido.nextval, 1010, 2, 13000);
